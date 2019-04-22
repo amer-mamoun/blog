7e80fceb782e5ec7f1ec80ff9ae6839480327897
Amer Mamoun <amer.mn.96@gmail.com>
2019-04-22 23:31:37 +0200

    Db megration

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\Controller;
 class UsersController extends Controller
 {
     public function __construct(){
-        $this->middleware('CheckAge'); 
+        $this->middleware( 'CheckAge');
     }
     public function listUsers(){
         return "list users - admin/users  controller";
